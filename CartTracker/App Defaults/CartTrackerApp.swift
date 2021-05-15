b9e2ee9b89d4bb818fde05d33181876bfa8d622c
Jaime Jazareno III <jaimejazareno@Codys-MacBook-Pro.local>
2021-05-15 20:14:38 +0800

    Update app defaults

--- CartTracker/App Defaults/CartTrackerApp.swift
+++ CartTracker/App Defaults/CartTrackerApp.swift
@@ -13,7 +13,7 @@ struct CartTrackerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ShopListView(name: .constant(""), budget: .constant(0.0))
+            ShopListView()
                 .environmentObject(store)
         }
     }
