73636a212131cc19130cd253fe5b2d1a520354ab
= <jaimejazarenoiii@gmail.com>
2021-06-13 14:34:42 +0800

    Add config for realm

--- CartTracker/App Defaults/AppDelegate.swift
+++ CartTracker/App Defaults/AppDelegate.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func applicationDidFinishLaunching(_ application: UIApplication) {
+        configRealm()
         if CommandLine.arguments.contains("testing") {
             // clear your app state before running UI tests here.
             UIView.setAnimationsEnabled(false)
         }
+    }
+
+    private func configRealm() {
+        let config = Realm.Configuration(readOnly: false, deleteRealmIfMigrationNeeded: true)
+        Realm.Configuration.defaultConfiguration = config
     }
 }
